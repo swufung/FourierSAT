fc5e1106cf8c203757d41d4de53d84a2e75c9c6e
Zhiwei Zhang <45185698+zzwonder@users.noreply.github.com>
2024-10-03 17:50:16 -0500

    Add files via upload

---
+++ newexp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for beta10 in $(seq 0 2 9);
+do
+         ./run.slurm randomInstances/cards fouriersat unconstrained_SQUARE_HJ_1.$beta10 1 square HJ 0 1.$beta10 solver
+         ./run.slurm randomInstances/cards fouriersat unconstrained_SQUARE_HJ_0.$beta10 1 square HJ 0 0.$beta10 solver
+         ./run.slurm randomInstances/CNF_1000 fouriersat unconstrained_SQUARE_HJ_0.$beta10 1 square HJ 0 0.$beta10 dev 
+         ./run.slurm randomInstances/XOR_1000 fouriersat unconstrained_SQUARE_HJ_0.$beta10 1 square HJ 0 0.$beta10 dev
+
+         ./run.slurm randomInstances/cards fouriersat unconstrained_ABS_HJ_1.$beta10 1 abs HJ 0 1.$beta10 solver
+         ./run.slurm randomInstances/cards fouriersat unconstrained_ABS_HJ_0.$beta10 1 abs HJ 0 0.$beta10 solver
+         ./run.slurm randomInstances/CNF_1000 fouriersat unconstrained_ABS_HJ_0.$beta10 1 abs HJ 0 0.$beta10 dev
+         ./run.slurm randomInstances/XOR_1000 fouriersat unconstrained_ABS_HJ_0.$beta10 1 abs HJ 0 0.$beta10 dev
+done     
+
